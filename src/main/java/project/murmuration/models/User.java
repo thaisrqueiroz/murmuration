fc4befc2b6906ae135bf7dbbcf3984eca33438b3
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-08-06 17:45:01 +0200

    feat(User): add constructor, getters and setters

---
+++ src/main/java/project/murmuration/models/User.java
@@ -0,0 +1,30 @@
+package project.murmuration.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String location;
+
+    private int balance;
+}
