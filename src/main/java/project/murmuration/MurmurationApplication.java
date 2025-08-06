3f9ae199194b22fe119a6830036c907ee2f9a7f4
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-08-06 17:42:32 +0200

    first commit

---
+++ src/main/java/project/murmuration/MurmurationApplication.java
@@ -0,0 +1,13 @@
+package project.murmuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurmurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurmurationApplication.class, args);
+	}
+
+}
