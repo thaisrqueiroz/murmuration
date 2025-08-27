5516806b335c00dbe65cc5df4b9d470d586ec342
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-08-27 13:15:06 +0200

    feat(TransactionService): add existsByOfferId condiction to the processTransaction logic

--- src/main/java/project/murmuration/transaction/TransactionRepository.java
+++ src/main/java/project/murmuration/transaction/TransactionRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByUserCustomerIdOrUserReceiverId(Long userCustomerId, Long userReceiverId);
+
+    boolean existsByOfferId(Long offerId);
 }
