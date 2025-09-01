7fe061fb2ec5b6ff8745a2709dfc14be3168bf51
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-09-01 12:21:54 +0200

    refactor(CategoryService, CategoryController): add duplicate checking to add a category, update JwtAuthFilter import

--- src/main/java/project/murmuration/category/CategoryRepository.java
+++ src/main/java/project/murmuration/category/CategoryRepository.java
@@ -3,9 +3,9 @@ package project.murmuration.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Object> findByName(String name);
+    boolean existsByName(String name);
+
+    boolean existsByNameAndId(String name, Long id);
 }
